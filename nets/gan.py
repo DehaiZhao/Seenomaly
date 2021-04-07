2efd490dc6b8b0c9a3d2cd79d970d00ae35097cd
Peter Vacouftsis <reteppetergcz@gmail.com>
2021-04-07 16:33:53 +1000

    Converted nets/gan to using tf-2 compatability mode

--- nets/gan.py
+++ nets/gan.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 import tf_slim as slim
 
